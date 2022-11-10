73fc8734d2a359f527b1e1373a4618cf41ab3b56
Abashjinn <a.trudel.photo@gmail.com>
2022-11-09 22:56:48 -0500

    Update agent.sh
    loop quick fix 2.0

--- agent.sh
+++ agent.sh
@@ -1,25 +1,19 @@
-#!/bin/env bash
+#!/bin/bash
 dir=$(/usr/bin/pwd)
 logname=logfile_ntfy.log
-((maxtime = 20))
-while [[ "$(date +%S)" != "00" ]]; do true; done
-while true; do
+runtime="1 minute"
+endtime=$(date -ud "$runtime" +%s)
 
-    sleep 30 &
-
-        sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
-        sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
-        sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \
-        sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
-        sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
-        sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
-        sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1;
-
-    ((delay = RANDOM % maxtime + 1))
-    ((maxtime += 1))
-    echo "$(date) Sleeping for ${delay} seconds (max ${maxtime})."
-    [[ ${delay} -gt 30 ]] && echo
-    sleep ${delay}
-    # Wait for timer to finish before next cycle.
-    wait
+while [[ $(date -u +%s) -le $endtime ]]
+do
+    echo "Time Now: `date +%H:%M:%S`"
+    echo "Sleeping for 30 seconds"
+    sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
+    sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
+    sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \
+    sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
+    sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
+    sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
+    sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1; \
+    sleep 30
 done

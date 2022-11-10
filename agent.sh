5f2097209c3255677d39d7c9ca6772bc3801b1b2
Abashjinn <a.trudel.photo@gmail.com>
2022-11-09 22:31:47 -0500

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -1,15 +1,25 @@
-#!/bin/bash
+#!/bin/env bash
 dir=$(/usr/bin/pwd)
 logname=logfile_ntfy.log
+((maxtime = 20))
+while [[ "$(date +%S)" != "00" ]]; do true; done
+while true; do
 
-for n in {1..2}; do currentTime=$(date +%s); \
-elapsedTime=$((currentTime - lastTime)); \
-if [ $elapsedTime -ge 5 ]; then \
-sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1; \
-sleep 30; lastTime=$currentTime; i=0; fi; done
+    sleep 30 &
+
+        sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
+        sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
+        sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \
+        sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
+        sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
+        sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
+        sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1;
+
+    ((delay = RANDOM % maxtime + 1))
+    ((maxtime += 1))
+    echo "$(date) Sleeping for ${delay} seconds (max ${maxtime})."
+    [[ ${delay} -gt 30 ]] && echo
+    sleep ${delay}
+    # Wait for timer to finish before next cycle.
+    wait
+done

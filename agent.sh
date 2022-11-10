6023737da59b7247ae6b6a02875fd50fde0ad36d
Abashjinn <a.trudel.photo@gmail.com>
2022-11-09 21:24:52 -0500

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -8,7 +8,7 @@ sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1 \
-&& sleep 29; \
+&& sleep 30; \
 sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \

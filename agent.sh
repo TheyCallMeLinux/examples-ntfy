e6062ea48d3c908645afd8ba0feec53294e3b52d
Abashjinn <a.trudel.photo@gmail.com>
2022-11-09 22:23:38 -0500

    Update agent.sh
    Quick fix on my messed up loop

--- agent.sh
+++ agent.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 dir=$(/usr/bin/pwd)
 logname=logfile_ntfy.log
-for i in {1..2}; do \
-sleep 15;
+
+for n in {1..2}; do currentTime=$(date +%s); \
+elapsedTime=$((currentTime - lastTime)); \
+if [ $elapsedTime -ge 5 ]; then \
 sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1;
-sleep 15;
-done
+sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1; \
+sleep 30; lastTime=$currentTime; i=0; fi; done

4dd1654ba4bfe6f873c6a17f7b82f158332a4902
Abashjinn <a.trudel.photo@gmail.com>
2022-11-09 23:28:06 -0500

    Update agent.sh
    going to sleep now it's semi-working 💤

--- agent.sh
+++ agent.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 dir=$(/usr/bin/pwd)
 logname=logfile_ntfy.log
-runtime="1 minute"
-endtime=$(date -ud "$runtime" +%s)
-
-while [[ $(date -u +%s) -le $endtime ]]
-do
-    echo "Time Now: `date +%H:%M:%S`"
-    echo "Sleeping for 30 seconds"
-    sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
-    sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
-    sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \
-    sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
-    sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
-    sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
-    sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1; \
-    sleep 30
+#!/bin/env bash
+while [ true ]; do
+sleep 30
+sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
+sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
+sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \
+sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
+sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
+sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
+sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1; \
 done

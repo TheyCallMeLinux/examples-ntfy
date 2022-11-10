12a842c31be6e7dfd25c99c91182e1b2faa114e6
Abashjinn <a.trudel.photo@gmail.com>
2022-11-09 22:03:55 -0500

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 dir=$(/usr/bin/pwd)
 logname=logfile_ntfy.log
+for i in {1..2}; do \
+sleep 30;
 sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1 \
-&& sleep 30; \
-sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
-sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1; \
+sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1;
+done

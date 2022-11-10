4d37ea31bcb4360e5158b9d27c12d7e2910e724c
Abashjinn <a.trudel.photo@gmail.com>
2022-11-09 19:24:18 -0500

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -4,6 +4,7 @@ logname=logfile_ntfy.log
 sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \
+sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1 \
@@ -11,6 +12,7 @@ sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1 \
 sh "$dir"/Disks.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/CPUload.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Docker.sh >> "$dir"/"$logname" 2>&1; \
+sh "$dir"/Jelly.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Used-RAM.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Process.sh >> "$dir"/"$logname" 2>&1; \
 sh "$dir"/Flood.sh >> "$dir"/"$logname" 2>&1; \
